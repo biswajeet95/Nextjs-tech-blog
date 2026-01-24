3ef0e2b012809ebc09d86b3b4bb358ade3db479b
Biswajeet Moharana <bmoharana95@gmail.com>
2026-01-24 15:39:34 +0530

    style

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /* config options here */
   reactCompiler: true,
-
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'iqraconsultancy.in',
-          },
-        ],
-        destination: 'https://www.iqraconsultancy.in/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
